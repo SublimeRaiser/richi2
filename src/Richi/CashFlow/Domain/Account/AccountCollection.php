865d5c6ecb41ed31b2b3fda9e32580d066d7a853
Nikolay Ryabkov <nikolay.ryabkov@sibers.com>
2021-09-18 20:21:32 +0700

    add collection class for account entity

---
+++ src/Richi/CashFlow/Domain/Account/AccountCollection.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Richi\CashFlow\Domain\Account;
+
+use Richi\CashFlow\Domain\AbstractDomainEntityCollection;
+
+/**
+ * @author Nikolay Ryabkov <ZeroGravity.82@gmail.com>
+ */
+final class AccountCollection extends AbstractDomainEntityCollection
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getEntityClass(): string
+    {
+        return Account::class;
+    }
+}
